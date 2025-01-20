1adc25ee5613957b69513418066c59fe00fec3fe
retha cindy aulia <127622712+Rethacindyaulia@users.noreply.github.com>
2025-01-20 15:27:51 +0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors'); 
+const authRoutes = require('./routes/authRoutes');
+const todoRoutes = require('./routes/todoRoutes');
+const profileRoutes = require('./routes/profileRoutes');
+const setupSwagger = require('./swagger');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || '0.0.0.0';
+
+app.use(cors()); 
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/todos', todoRoutes);
+app.use('/api/profile', profileRoutes);
+
+setupSwagger(app);
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
+// testing api
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    succcess: true,
+    message: "API is working",
+  });
+});
+
+// unknown route
+app.all("*", (req, res, next) => {
+  const err = new Error(`Route ${req.originalUrl} not found`);
+  err.statusCode = 404;
+  next(err);
+});
